0162b7bfed53bf7e42980a25160ef682587bf3dc
Brandon Chin <brandonchin@me.com>
2018-02-13 19:00:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   reversed_array.join(" ")
 end
+
+def reverse_each_word(string)
+  new_array = string.split
+  reversed_array = []
+  new_array.collect do |reversed|
+    reversed_array << reversed.reverse
+  end
+  reversed_array.join(" ")
+end
